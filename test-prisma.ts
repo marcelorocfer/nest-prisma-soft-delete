67d7ab2427d678ee554389354dba49cca27e16d8
marcelorocfer <marceloengecomp@gmail.com>
2026-04-29 17:00:54 -0300

    chore: remove test files

--- test-prisma.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-console.log('Keys in PrismaClient:', Object.keys(prisma).filter(k => !k.startsWith('_') && !k.startsWith('$')));
